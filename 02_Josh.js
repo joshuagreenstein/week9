671f10fa63a6696cfa15ba93d2dfa2b2b2e0d74d
joshuagreenstein <joshuagreenstein@seattleacademy.org>
2016-02-09 14:58:02 -0800

    added new 02_josh.js

--- 02_Josh.js
+++ 02_Josh.js
@@ -1,5 +1,16 @@
-console.log(process.argv)
-var pin = process.argv[2]
-var status = process.argv[3]
+var pin = process.argv[2]*1
+console.log(pin);
+var wpi = require('wiring-pi');
 
-console.log("pin number " + pin + " turn " + status)
+//console.log('wpi',wpi);
+wpi.setup('gpio');
+
+wpi.pinMode(pin,wpi.OUTPUT);
+
+var value = 1;
+
+function toggleBulb(){
+	wpi.digitalWrite(pin, value);
+	value =+!value;
+}
+setInterval(toggleBulb, 500);
